4e64447e5a82be62a7db2d3b9e8862e806db933d
서민주 <sminju1009@gmail.com>
2024-03-27 11:31:05 +0900

    [BE][민주]책 정보 조회 api 생성 완료

--- back/KEAD/src/main/java/com/example/pppp/Book/dto/response/BookResponseDto.java
+++ back/KEAD/src/main/java/com/example/pppp/Book/dto/response/BookResponseDto.java
@@ -1,38 +1,24 @@
-package com.example.pppp.Book.dto.response;
+package com.example.pppp.book.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookResponseDto {
-    private Integer book_id;
-    private String book_name;
-    private String img_url;
+    private int bookId;
+    private String bookName;
+    private String imgUrl;
     private String author;
     private String publisher;
-    private Integer genre;
+    private int genre;
     private String description;
     private String isbn;
-    private String message; // 실패 시 메시지 필드 추가
 
-    // 생성자 오버로딩하여 성공 및 실패 시에 대한 생성자 추가
-    public BookResponseDto(Integer book_id, String book_name, String img_url, String author, String publisher, Integer genre, String description, String isbn) {
-        this.book_id = book_id;
-        this.book_name = book_name;
-        this.img_url = img_url;
-        this.author = author;
-        this.publisher = publisher;
-        this.genre = genre;
-        this.description = description;
-        this.isbn = isbn;
-    }
-
-    public BookResponseDto(String message) {
-        this.message = message;
-    }
 }
