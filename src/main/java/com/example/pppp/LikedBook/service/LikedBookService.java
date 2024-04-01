ef91acd3a49ac8d3d859f4998f70dd2eb365e08d
위세영 <wiese0630@naver.com>
2024-04-01 14:44:03 +0900

    [BE][세영] 좋아요 api 수정 완룧

--- src/main/java/com/example/pppp/LikedBook/service/LikedBookService.java
+++ src/main/java/com/example/pppp/LikedBook/service/LikedBookService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service // 빈으로 등록
@@ -18,10 +19,18 @@ public class LikedBookService {
     private final LikedBookRepository likedBookRepository;
     // 좋아요 추가 메서드
     public LikedBook save(AddLikedBookRequest request) {
+        // 이미 해당 사용자가 해당 책을 좋아하는지 확인
+        Optional<LikedBook> existingLikedBook = likedBookRepository.findByBookIdAndMemberId(request.getBookId(), request.getMemberId());
 
-        return likedBookRepository.save(request.toEntity());
+        // 이미 좋아하는 책이 있는 경우 좋아요 취소
+        if (existingLikedBook.isPresent()) {
+            likedBookRepository.delete(existingLikedBook.get());
+            return null; // 좋아요 취소되었으므로 반환할 값이 없음
+        }
+        // 새로운 좋아하는 책 생성 및 저장
+        LikedBook likedBook = new LikedBook(request.getBookId(), request.getMemberId());
+        return likedBookRepository.save(likedBook);
     }
-
 
     // BookId와 MemberId를 이용하여 좋아요를 삭제하는 메서드
     @Transactional
