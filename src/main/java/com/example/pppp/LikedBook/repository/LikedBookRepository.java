4b0de38da36bbcc677688ff90e6cbdba5df4fd22
위세영 <wiese0630@naver.com>
2024-04-01 14:20:23 +0900

    [BE][세영]좋아요 조회 api 개발 완료

--- src/main/java/com/example/pppp/LikedBook/repository/LikedBookRepository.java
+++ src/main/java/com/example/pppp/LikedBook/repository/LikedBookRepository.java
@@ -4,9 +4,12 @@ import com.example.pppp.LikedBook.entity.LikedBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LikedBookRepository extends JpaRepository<LikedBook, Integer> {
     // BookId와 MemberId를 이용하여 LikedBook을 삭제하는 메서드
     void deleteByBookIdAndMemberId(int bookId, int memberId);
 
+    List<LikedBook> findByMemberId(int memberId);
 }
--- src/main/java/com/example/pppp/LikedBook/service/LikedBookService.java
+++ src/main/java/com/example/pppp/LikedBook/service/LikedBookService.java
@@ -6,6 +6,8 @@ import com.example.pppp.LikedBook.repository.LikedBookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service // 빈으로 등록
 public class LikedBookService {
@@ -20,5 +22,9 @@ public class LikedBookService {
     // BookId와 MemberId를 이용하여 좋아요를 삭제하는 메서드
     public void deleteByBookIdAndMemberId(int bookId, int memberId) {
         likedBookRepository.deleteByBookIdAndMemberId(bookId, memberId);
+    }
+
+    public List<LikedBook> getLikedBooksByUserId(int userId) {
+        return likedBookRepository.findByMemberId(userId);
     }
 }
