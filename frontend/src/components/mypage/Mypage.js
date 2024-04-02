d04dee9bcf4d4134e50c87471fb794381ebee262
kennedy1234 <kennedy1234@naver.com>
2024-04-02 16:42:27 +0900

    [FE][승현] profile fix

--- frontend/src/components/mypage/Mypage.js
+++ frontend/src/components/mypage/Mypage.js
@@ -1,14 +1,51 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import './Mypage.css'
-
+import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
+import useAuthStore from '../../store/authStore';
+
 
 function Mypage() {
+  const { isAuthenticated, user, logout: logoutFromStore } = useAuthStore();
+  const [userInfo, setUserInfo] = useState({ nickname: '' })
   const navigate = useNavigate();
+
+ // 페이지 로드 시 사용자 정보를 가져오는 함수
+ useEffect(() => {
+  if (!isAuthenticated) {
+    navigate('/login');
+    return;
+  }
+
+  const fetchUserInfo = async () => {
+    try {
+      const token = localStorage.getItem('jwtToken'); // 로컬 스토리지에서 JWT 토큰 가져오기
+      // console.log(token) 토큰있음
+      const response = await axios.get('http://localhost:8080/users/me', {
+        headers: {
+          Authorization: `Bearer ${token}` // 요청 헤더에 토큰 포함
+        }
+      });
+      setUserInfo(response.data);
+      console.log(response.data); // 응답 데이터를 콘솔에 출력
+    } catch (error) {
+      console.error('Error fetching user info:', error);
+      // 인증 실패 시 로그인 페이지로 리다이렉트 등의 처리를 할 수 있음
+    }
+  };
+
+  fetchUserInfo();
+}, []); // 의존성 배열을 비워서 컴포넌트 마운트 시 1회만 호출되도록 함
 
   const goToEditProfile = () => {
     navigate('/profile')
   }
+
+  const logout = () => {
+    localStorage.removeItem('jwtToken'); // 로컬 스토리지에서 토큰 삭제
+    navigate('/login'); // 로그인 페이지로 리다이렉트
+  };
 
   return (
     <div>
@@ -23,7 +60,7 @@ function Mypage() {
       </h2>
       <div className='mypageTop'>
         <div> {/* 이름과 Joined 정보를 하나의 div로 묶습니다. */}
-          <h3 className='mynameFontSize'>호날두</h3>
+          <h3 className='mynameFontSize'>{ userInfo.nickname }</h3>
           <div className="flex-container">
             <div className="flex-item">
               <img alt="" src="img/Vector.png" />
@@ -70,7 +107,7 @@ function Mypage() {
       <div className='background-orange mypage-box' onClick={goToEditProfile}>
         회원정보수정
       </div>
-      <div className='background-orange mypage-box'>
+      <div className='background-orange mypage-box' onClick={logout}>
         로그아웃
       </div>
     </div>
