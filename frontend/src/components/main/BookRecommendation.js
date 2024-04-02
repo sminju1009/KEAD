385f2600f14e28813514431cd92584f058a127ba
선상범 <gksgusrhks12@naver.com>
2024-04-02 15:30:25 +0900

    추천이미지 완료

--- frontend/src/components/main/BookRecommendation.js
+++ frontend/src/components/main/BookRecommendation.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // React Router의 Link 컴포넌트 추가
 import './BookRecommendation.css';
 
 function BookRecommendation() {
   return (
-    <div className='background-orange mypage-box'>
+    <Link to="/book_recomm" className='background-orange mypage-box'>
       호날두님을 위한 추천도서
-    </div>
+    </Link>
   );
 }
 
