55337cde4b83aea68634a479cf2969d0b20503f6
선상범 <gksgusrhks12@naver.com>
2024-04-02 16:21:44 +0900

    이름변경

--- frontend/src/components/main/BookRecommendation.js
+++ frontend/src/components/main/BookRecommendation.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // React Router의 Link 컴포넌트 추가
+import React, { useState, useEffect } from 'react'; // React와 useEffect를 임포트합니다.
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './BookRecommendation.css';
 
 function BookRecommendation() {
+  const [userInfo, setUserInfo] = useState({ nickname: '' });
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const token = localStorage.getItem('jwtToken');
+        const response = await axios.get('http://j10c106.p.ssafy.io:8082/users/me', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        setUserInfo(response.data);
+      } catch (error) {
+        console.error('Error fetching user info or book data:', error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
   return (
     <Link to="/book_recomm" className='background-orange mypage-box'>
-      호날두님을 위한 추천도서
+      {userInfo.nickname}을 위한 추천도서
     </Link>
   );
 }
